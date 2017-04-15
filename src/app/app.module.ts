01be20338aae3550722b60ac035457f2cb7db69a
carloscalderon <cdcalderon@gmail.com>
2017-04-14 23:14:50 -0600

    Add shopping list service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import {TabsPage} from "../pages/tabs/tabs";
 import {RecipesPage} from "../pages/recipes/recipes";
 import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
+import {ShoppingListService} from "../service/shopping-list";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService
   ]
 })
 export class AppModule {}
