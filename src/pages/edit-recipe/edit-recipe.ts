1ff8f58f5688b360dbac3614c18836ee9ff57f7f
carloscalderon <cdcalderon@gmail.com>
2017-04-17 02:25:38 -0500

    Add initializeForm method in edit recipe page

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { NavParams } from "ionic-angular";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'page-edit-recipe',
@@ -8,11 +9,22 @@ import { NavParams } from "ionic-angular";
 export class EditRecipePage implements  OnInit{
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
+
   constructor(private navParams: NavParams) {
 
   }
 
   ngOnInit(): void {
     this.mode = this.navParams.get('mode');
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+        'title': new FormControl(null, Validators.required),
+        'description': new FormControl(null, Validators.required),
+        'difficulty': new FormControl('Medium', Validators.required)
+      }
+    )
   }
 }
