a02213a42256e5090b4db5d8010f9ec859152bbf
carloscalderon <cdcalderon@gmail.com>
2017-04-21 03:28:22 -0500

    Add ingredients to recipe, implement remove recipe

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {NavController, NavParams} from "ionic-angular";
 import {Recipe} from "../../models/recipe";
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {ShoppingListService} from "../../service/shopping-list";
+import {RecipesService} from "../../service/recipes";
 
 @Component({
   selector: 'pag-recipe',
@@ -12,7 +14,10 @@ export class RecipePage implements OnInit {
   recipe: Recipe;
   index: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private slService: ShoppingListService,
+              private recipesService: RecipesService) {
 
   }
 
@@ -23,5 +28,14 @@ export class RecipePage implements OnInit {
 
   onEditRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'Edit', recipe: this.recipe, index: this.index});
+  }
+
+  onAddIngredients() {
+    this.slService.addItems(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipesService.removeRecipe(this.index);
+    this.navCtrl.popToRoot();
   }
 }
